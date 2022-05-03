2b80c36198d93241a0aaf601bccb6093e0cd1fa2
Micah Norrington <manorrington@gmail.com>
2022-05-02 21:31:57 -0400

    added files

--- gi3/script.js
+++ gi3/script.js
@@ -1,55 +1,89 @@
 // //Easy
-// const run =(exercise) => `Today's exercise: ${exercise}`
-// const swim =(exercise) => `Today's exercise:  ${exercise}`    
+// const run =(exercise) => `Today's exercise: ${exercise}`;
+// const swim =(exercise) => `Today's exercise:  ${exercise}`;    
 
-// console.log(run(`running`))
-// console.log(swim(`swimming`))
+// console.log(run(`running`));
+// console.log(swim(`swimming`));
 
 
-// //Medium
+
+//Medium
 // let sharePizza = (slices, cutSlices) => slices / cutSlices;
-// console.log(`Each person gets ${sharePizza(8, 2)} slices`)
+// console.log(`Each person gets ${sharePizza(8, 2)} slices`);
 
 
-// //Hard
-// let func = () =>{
-//     function closure () {
-//         let pii = {
-//             name: ``,
-//             ssn: `000-00-0000`,
-//             getName() {
-//                 return this.name
-//             }
-//         };
-//         return pii.getName()
+
+//Hard
+// class Pii {
+//     name = `john`;
+//     #priv = 121321; 
+
+//     getName() {
+//         return this.name;
 //     }
-//     return closure
-// } 
-// console.log(closure())
+// }
+
+// let pii = new Pii();
+// console.log(pii.getName());
 
 
 
 // //Very Hard
-function Person(name, job, age){
-    this.name = name;
-    this.job = job;
-    this.age = age;
-}
-Person.prototype.exercise = String(`running is fun!`)
-Person.prototype.fetchJob = String(`John is a Data Developer`)
+// function Person(name, job, age) {
+//     this.name = name;
+//     this.job = job;
+//     this.age = age;
+// }
 
-function Programmer(...args){
-    Person.apply(this, args)
-    this.languages = this.languages
-    this.isBusy = true;
-}
-Programmer.prototype.languages = String(`English, Japanse, Spanish`)
+// Person.prototype.exercise = String(`running is fun!`);
+// Person.prototype.fetchJob = String(`John is a Data Developer`);
 
 
-let person1 = new Person(`John`, `Developer`, 21)
-let programmer1 = new Programmer(`Melissa`, `Programmer`, 29, `E`)
+// function Programmer(...args) {
+//     Person.apply(this, args);
+//     this.languages = this.languages;
+//     this.isBusy = true;
+// }
 
-console.log(person1, programmer1)
+// Programmer.prototype.languages = String(`HTML, CSS, Javascript`);
+
+// Programmer.prototype.completeTask = function() {
+//     this.isBusy = false;
+// }
+
+// Programmer.prototype.acceptNewTask = function() {
+//     this.isBusy = true;
+// }
+
+// Programmer.prototype.offerNewTask = function() {
+//     if(this.isBusy === true) {
+//         console.log(`Man, I'm so busy`);
+//     } else if (this.isBusy === false) {
+//         console.log(`Whew, I sure could do some work`);
+//     }
+// }
+
+// Programmer.prototype.learnLanguage = function() {
+//     console.log(`Python, SQL`);
+// }
+
+// Programmer.prototype.listLanguage = function() {
+//     console.log(`HTML, CSS, Javascript`)
+// }
+
+// // function Apprentice(...args){
+// //     Programmer.apply(this, args);
+// // }
+// // Apprentice.prototype.languages = String(`HTML, CSS`);
+// // ^ passed in another person to test if it worked
+
+
+// let person1 = new Person(`John`, `Developer`, 21);
+// let programmer1 = new Programmer(`Melissa`, `Programmer`, 29,);
+// // let apprentice = new Apprentice(`Micah`, `Apprenctice`, 19);
+
+// console.log(person1, programmer1, /* apprentice */);
+
    
 
 
